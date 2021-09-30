2ae6e186a0aea40296eb31f34fc79fadd6ec8832
nicorendon02 <nrendon87351@umanizales.edu.co>
2021-09-30 00:34:18 -0500

    images works!

--- src/app/shared/vertical-menu/vertical-menu-routes.config.ts
+++ src/app/shared/vertical-menu/vertical-menu-routes.config.ts
@@ -9,25 +9,17 @@ export const ROUTES: RouteInfo[] = [
     path: '/prueba', title: 'Calculator', icon: 'icon-calculator', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
   },
   {
-    path: '/challenge/images', title: 'Images', icon: 'icon-calculator', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
-  },
-  {
-    path: '/challenge/media-player', title: 'Media Player', icon: 'icon-calculator', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
-  },
-  {
-    path: '/challenge/text-editor', title: 'Text Editor', icon: 'icon-calculator', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
-  },
-  {
-    path: '', title: 'Menu Levels', icon: 'ft-align-left', class: 'has-sub', badge: '3', badgeClass: 'badge badge-pill badge-danger float-right mr-1 mt-1', isExternalLink: false,
+    path: '', title: 'Challenge', icon: 'ft-align-left', class: 'has-sub', badge: '3', badgeClass: 'badge badge-pill badge-danger float-right mr-1 mt-1', isExternalLink: false,
     submenu: [
-        { path: '/YOUR-ROUTE-PATH', title: 'Second Level', icon: 'ft-arrow-right submenu-icon', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
-        {
-            path: '', title: 'Second Level Child', icon: 'ft-arrow-right submenu-icon', class: 'has-sub', badge: '', badgeClass: '', isExternalLink: false,
-            submenu: [
-                { path: '/YOUR-ROUTE-PATH', title: 'Third Level 1.1', icon: 'ft-arrow-right submenu-icon', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
-                { path: '/YOUR-ROUTE-PATH', title: 'Third Level 1.2', icon: 'ft-arrow-right submenu-icon', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
-            ]
-        },
+      {
+        path: '/challenge/images', title: 'Images', icon: 'icon-calculator', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
+      },
+      {
+        path: '/challenge/media-player', title: 'Media Player', icon: 'icon-calculator', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
+      },
+      {
+        path: '/challenge/text-editor', title: 'Text Editor', icon: 'icon-calculator', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
+      }
     ]
 },
 ];
